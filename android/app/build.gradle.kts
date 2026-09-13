b3e781b0335833589b487abc7bd0b450fdcf0665
Ddmmiittrriiyy <98453132+Ddmmiittrriiyy@users.noreply.github.com>
2026-09-13 09:37:51 +0100

    Add Android alarm core foundation

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace = "com.voicetasks.next"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.voicetasks.next"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
